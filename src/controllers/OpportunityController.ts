81940e981982d3578d3a0bf17001e00476d09fb2
Gabrielncosta <gabrielncosta208@gmail.com>
2020-09-05 17:39:01 -0300

    refac: opportunity controller calls order repository

--- src/controllers/OpportunityController.ts
+++ src/controllers/OpportunityController.ts
@@ -2,14 +2,21 @@ import { Request, Response } from 'express';
 
 import PipedriveService from '../services/PipedriveService';
 import BlingService from '../services/BlingService';
+import OrderRepository from '../repositories/OrderRepository';
 
 export default class OpportunityController {
   public async create(request: Request, response: Response): Promise<Response> {
     const Pipedrive = new PipedriveService();
     const Bling = new BlingService();
+    const orderRepository = new OrderRepository();
 
     const wonDeals = await Pipedrive.execute();
+
     const orders = await Bling.execute(wonDeals);
+
+    orders.map(async order => {
+      await orderRepository.create(order);
+    });
 
     return response.json({ ok: true });
   }
