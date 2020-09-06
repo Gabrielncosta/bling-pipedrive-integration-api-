a04f91de9d1f958091ffc5f88e4aaee18887f01c
Gabrielncosta <gabrielncosta208@gmail.com>
2020-09-06 20:34:50 -0300

    feat: pipedrive service error if no deals are found

--- src/services/PipedriveService.ts
+++ src/services/PipedriveService.ts
@@ -1,15 +1,24 @@
 import api from '../config/pipedriveApi';
 
+import ServiceError from '../errors/ServiceError';
+
 class PipedriveService {
   public async execute(): Promise<Array<string>> {
     const deals = await api.get('/deals', {
       params: {
-        status: 'won',
+        status: 'lost',
         api_token: process.env.API_TOKEN,
       },
     });
 
     const { data } = deals.data;
+
+    if (!data) {
+      throw new ServiceError(
+        'No deals with status won found on provided Pipedrive account',
+        400,
+      );
+    }
 
     return data;
   }
