6174275e26b2c9f83304070cbc7fa9c496f7ae7e
Gabrielncosta <gabrielncosta208@gmail.com>
2020-09-06 20:15:22 -0300

    Revert "refac: .env url in mongoose conection"
    This reverts commit 4c56abb2f815cf2127d4cb35bf2f47c679200962.

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-mongoose.connect(`${process.env.MONGO_URL}`, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(
+  'mongodb+srv://linkapi:9CXzufZkb6kGeFua@linkapi.kthb4.mongodb.net/dblinkapi?retryWrites=true&w=majority',
+  { useNewUrlParser: true, useUnifiedTopology: true },
+);
