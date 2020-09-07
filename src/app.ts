580b25b2e799a1105b4e6f571f99e4fdcc756aee
Gabrielncosta <gabrielncosta208@gmail.com>
2020-09-06 21:18:25 -0300

    global error handler

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import 'reflect-metadata';
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
 import routes from './routes';
+import AppError from './helpers/errors/AppError';
 
 import './database/connection';
 
@@ -8,5 +10,20 @@ const app = express();
 
 app.use(express.json());
 app.use(routes);
+
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      message: err.message,
+    });
+  }
+
+  console.error(err);
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
+});
 
 export default app;
